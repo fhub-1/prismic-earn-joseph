6b5684d2ef3a2914f517ba0db0755650ca695880
fhub-1 <flutterhub1.21.0@gmail.com>
2024-02-16 22:22:49 +0300

    new: working on my skills with prismic

---
+++ src/slices/Hero/index.tsx
@@ -0,0 +1,24 @@
+import { Content } from "@prismicio/client";
+import { SliceComponentProps } from "@prismicio/react";
+
+/**
+ * Props for `Hero`.
+ */
+export type HeroProps = SliceComponentProps<Content.HeroSlice>;
+
+/**
+ * Component for "Hero" Slices.
+ */
+const Hero = ({ slice }: HeroProps): JSX.Element => {
+  return (
+    <section
+      data-slice-type={slice.slice_type}
+      data-slice-variation={slice.variation}
+    >
+      {/* Placeholder component for hero (variation: {slice.variation}) Slices */}
+      {slice.primary.first_nmae}
+    </section>
+  );
+};
+
+export default Hero;
