15fecd91bfbdde5de1745396c5805027c8c62894
fhub-1 <flutterhub1.21.0@gmail.com>
2024-02-24 11:14:28 +0300

    adding

--- src/components/ContentBody.tsx
+++ src/components/ContentBody.tsx
@@ -4,29 +4,13 @@ import { Content, DateField, isFilled } from "@prismicio/client";
 import { components } from "@/slices";
 import Heading from "@/components/Heading";
 import Bounded from "@/components/Bounded";
+import { createClient } from "@/prismicio";
 
 export default function ContentBody({
   page,
 }: {
   page: Content.BlogPostDocument | Content.ProjectDocument;
 }) {
-  function formDate(date: DateField) {
-    if (isFilled.date(date)) {
-      const dateOptions: Intl.DateTimeFormatOptions = {
-        weekday: "long",
-        year: "numeric",
-        month: "long",
-        day: "numeric",
-      };
-
-      return new Intl.DateTimeFormat("en-ke", dateOptions).format(
-        new Date(date)
-      );
-    }
-  }
-
-  const formattedDate = formDate(page.data.date);
-
   return (
     <Bounded as="article">
       <div className="rounded-2xl border-2 border-slate-800 bg-slate-900 px-4 py-10 md:px-8 md:py-20">
@@ -39,7 +23,7 @@ export default function ContentBody({
           ))}
         </div>
         <p className="mt-8 border-b border-slate-600 text-xl font-medium text-slate-300">
-          {formattedDate}
+          {/* {formatedDate} */}
         </p>
         <div className="prose prose-lg prose-invert mt-12 w-full max-w-none md:mt-20">
           <SliceZone slices={page.data.slices} components={components} />
