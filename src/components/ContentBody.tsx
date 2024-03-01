05c545fbcece20fc8a1a8a1f11e2c76a057a02ec
fhub-1 <flutterhub1.21.0@gmail.com>
2024-03-01 22:16:34 +0300

    working on bug:

--- src/components/ContentBody.tsx
+++ src/components/ContentBody.tsx
@@ -25,6 +25,7 @@ export default function ContentBody({
         </div>
         <p className="mt-8 border-b border-slate-600 text-xl font-medium text-slate-300">
           {formattedDate}
+          {/* working on bug */}
         </p>
         <div className="prose prose-lg prose-invert mt-12 w-full max-w-none md:mt-20">
           <SliceZone slices={page.data.slices} components={components} />
